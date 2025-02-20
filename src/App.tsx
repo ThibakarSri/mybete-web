042bef2175eeb24f67b1155d7119fa4d080cd6e4
Sanjana <asanjana0823@gmail.com>
2025-02-20 15:37:52 +0530

    Add hero file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import Header from './components/Header';
-import Hero from '';
+import Hero from './components/Hero';
 import Features from '';
 import Footer from '';
 
